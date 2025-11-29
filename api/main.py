d08c0feb8e27053e0b1b60962cc190b6101da4b1
gchuinard <45691364+gchuinard@users.noreply.github.com>
2025-11-29 01:29:31 +0100

    Dockerisation front/back

--- api/main.py
+++ api/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],        # Autoriser toutes les origines (DEV)
+    allow_credentials=True,
+    allow_methods=["*"],        # Autoriser tous les verbes HTTP
+    allow_headers=["*"],        # Autoriser tous les headers
+    expose_headers=["*"],       # Expose tous les headers
+)
 
 @app.get("/health")
 def health():
--- front/svelte.config.js
+++ front/svelte.config.js
@@ -1,8 +1,7 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import preprocess from "svelte-preprocess";
 
-/** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+  preprocess: preprocess({
+    typescript: true
+  })
+};
