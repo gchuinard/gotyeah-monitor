3f4b678cf6a94fb93376006e32550ff4876298aa
gchuinard <45691364+gchuinard@users.noreply.github.com>
2026-03-09 23:19:11 +0100

    add viteconfig

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  preview: {
+    allowedHosts: ['monitor.gautierchuinard.com']
+  },
   plugins: [
     tailwindcss(),
     sveltekit()
